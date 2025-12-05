d841cc3e084c7a910e0c7a666665a0773768c77f
Denis Mikhailov <denis.mikhailov@lottoland.com>
2025-12-05 10:20:42 +0100

    fix(mongodb): Refactor MongoDB client connection logic to improve global promise handling

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -14,23 +14,17 @@ const options: MongoClientOptions = {
   },
 };
 
-let client: MongoClient;
+const globalWithMongo = global as typeof globalThis & {
+  _mongoClientPromise?: Promise<MongoClient>;
+};
+
 let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === 'development') {
-  const globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise?: Promise<MongoClient>;
-  };
-
-  if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    globalWithMongo._mongoClientPromise = client.connect();
-  }
-  clientPromise = globalWithMongo._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+if (!globalWithMongo._mongoClientPromise) {
+  const client = new MongoClient(uri, options);
+  globalWithMongo._mongoClientPromise = client.connect();
 }
+clientPromise = globalWithMongo._mongoClientPromise;
 
 export async function getDatabase(): Promise<Db> {
   const client = await clientPromise;
